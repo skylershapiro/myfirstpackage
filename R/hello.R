e8aae9c66bd64451487d8cc2a3f6287947149a0f
Skyler Shapiro <skyler.shapiro@gmail.com>
2021-06-02 10:49:22 -0400

    commitment

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -13,6 +13,8 @@
 #   Check Package:             'Cmd + Shift + E'
 #   Test Package:              'Cmd + Shift + T'
 
-hello <- function() {
-  print("Hello, world!")
-}
+  hello <- function(name = "your name") {
+    name <- stringr::str_to_title(name)
+    print(paste("Hello,", name))
+  }
+
